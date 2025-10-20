4f066138ed30be834643813231137afbbca5cb78
julioOv-1104 <julioovalle202232237@cunoc.edu.gt>
2025-10-20 17:35:40 -0600

    Creacion de la vista para el usuario especial

--- proyecto_2_IPC2_2025/src/app/vista-especial/vista-especial.ts
+++ proyecto_2_IPC2_2025/src/app/vista-especial/vista-especial.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-vista-especial',
@@ -19,6 +20,11 @@ precioTexto: number=200;
 precioImagen: number=300;
 precioVideo: number = 450;
 total: number = 0;
+textoAnuncio: String = "";
+urlAnuncio: String ="";
+videoSeguro?: SafeResourceUrl; // 👈 tipo especial para iframe
+
+  constructor(private sanitizer: DomSanitizer) {}
 
 mostrarComprarTexto() {
   this.mostrarFormularioTexto = !this.mostrarFormularioTexto;
@@ -40,4 +46,24 @@ mostrarTotalCompra(){
 this.total = this.precioPorDia * Number(this.periodoSeleccionado);
 }
 
+getVideoUrl() {
+  if (!this.urlAnuncio) return '';
+
+  let videoId = '';
+
+  if (this.urlAnuncio.includes('youtube.com')) {
+    // toma el texto después de "v=" y corta cualquier otro parámetro (&)
+    videoId = this.urlAnuncio.split('v=')[1].split('&')[0];
+  } else if (this.urlAnuncio.includes('youtu.be')) {
+    // toma el último segmento del enlace
+    videoId = this.urlAnuncio.split('/').pop()?.split('?')[0] || '';
+  }
+
+  const embedUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&loop=1&playlist=${videoId}`;
+  this.videoSeguro = this.sanitizer.bypassSecurityTrustResourceUrl(embedUrl);
+  return this.videoSeguro;
 }
+
+}
+
+
